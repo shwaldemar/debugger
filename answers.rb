da5c80960084c457ff3c4427b1e845358ad452b4
Simon <shwaldemar@gmail.com>
2019-07-25 09:07:28 +0100

    answers added

---
+++ answers.rb
@@ -0,0 +1,18 @@
+What is the purpose of a breakpoint?
+Breakpoints are source code markers that let you suspend program execution at a specific point and examine its behaviour.
+
+Does the line of code on a breakpoint run when you start debugging?
+No the break point pauses on the line you selected and opens the debugging console which offers you a number of debugger options.
+
+How do we debug the next line of code?
+Use the Step-over option  run-in of code & move to next line.
+
+What does the step into command do?
+The step-into option allows you to inspect the elements of the code and follow the function and inspect the elements it calls.
+
+What is the difference between evaluate expression and evaluate code fragment?
+
+Evaluate Expression enables you to evaluate complex expressions, such as method calls, operator expressions, lambda expressions, and anonymous classes.
+
+Evaluate code fragment allows you to try out different expressions in the same window,
+and displays the result of the last expression. This mode allows you to write ad hoc debugging methods.
